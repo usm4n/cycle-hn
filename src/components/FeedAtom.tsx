84069207a74a724a22bdc7d888498934392ccaf6
Usman Riaz <usmanri4z@gmail.com>
2017-10-19 22:02:40 +0500

    little fixes on feed display

--- src/components/FeedAtom.tsx
+++ src/components/FeedAtom.tsx
@@ -26,15 +26,37 @@ export type Reducer = (prev: State) => State;
 export type FeedSinks = Sinks & { onion: Stream<Reducer> };
 export type FeedSources = Sources & { onion: StateSource<State> };
 
+function url(feed: State): VNode {
+    if (feed.url && feed.url.startsWith('http')) {
+        return <a className="feed-title" href={feed.url}>{feed.title}</a>;
+    } else {
+        return <a className="feed-title" href={`/atom/${feed.id}`}>{feed.title}</a>;
+    }
+}
+
+function domain(feed: State): any {
+    return feed.domain && <span className="feed-domain">({feed.domain})</span>;
+}
+
+function author(feed: State): any {
+    return feed.type !== 'job' && <span className="feed-author"> by {feed.user}</span>;
+}
+function points(feed: State): any {
+    return feed.type !== 'job' && <span> {feed.points} points </span>;
+}
+function commentsLink(feed: State): any {
+    return feed.type !== 'job' && <a href={`/atom/${feed.id}`}> | {feed.comments_count} comments </a>;
+}
+
 function view(state$: Stream<State>): Stream<VNode> {
     return state$.map(feed =>
         <div className="feed">
-            <a className="feed-title" href={feed.url}>{feed.title}</a>
-            <span className="feed-domain">({feed.domain})</span>
+            {url(feed)}
+            {domain(feed)}
             <div className="feed-footer">
-                <span> {feed.points} points </span> by
-                <span className="feed-author"> {feed.user}</span>
-                <span> {feed.time_ago} | <a href={`/atom/${feed.id}`}>{feed.comments_count} comments </a></span>
+                {points(feed)}
+                {author(feed)}
+                <span> {feed.time_ago} {commentsLink(feed)}</span>
             </div>
         </div>
     );

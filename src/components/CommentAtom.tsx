b9f1fdc2080aa51cb5d322cc7ae32abbebbab28d
Usman Riaz <usmanri4z@gmail.com>
2017-10-19 17:39:45 +0500

    added threaded comments

--- src/components/CommentAtom.tsx
+++ src/components/CommentAtom.tsx
@@ -4,8 +4,10 @@ import {
     Component
 } from '../interfaces';
 import { VNode } from '@cycle/dom';
+import isolate from '@cycle/isolate';
 import xs, { Stream } from 'xstream';
 import { StateSource } from 'cycle-onionify';
+import { CommentCollection } from './CommentCollection';
 
 export interface State {
     id: number;
@@ -28,23 +30,30 @@ function intent(sources: Sources): Stream<boolean> {
         .fold((show: boolean) => !show, true);
 }
 
-function view(state$: Stream<State>, action$: Stream<any>): Stream<VNode> {
-    return xs.combine(state$, action$)
-        .map(([comment, showComment]) =>
+function view(state$: Stream<State>, commentChildren$: Stream<VNode>, action$: Stream<any>): Stream<VNode> {
+    return xs.combine(state$, commentChildren$, action$)
+        .map(([comment, children, showComment]) =>
         <li className="comment">
             <div className={'comment-header ' + `${!showComment ? 'active' : ''}`}>
                 <span className="comment-author"> {comment.user} </span>
                 <span> {comment.time_ago} </span>
                 <span className="comment-hide">[{showComment ? '-' : '+'}]</span>
             </div>
-            {showComment && <div className="comment-content" innerHTML={comment.content}/>}
+            {showComment && <div className="comment-content" innerHTML={comment.content}>
+                <a className="reply" href={`https://news.ycombinator.com/reply?id=${comment.id}`}>reply</a>
+            </div>}
+            {(children && showComment) && <ul className="comment-list">
+                {children}
+            </ul>}
         </li>
     );
 }
 
 export const CommentAtom: Component = function(sources: CommentSources): CommentSinks {
     const action$ = intent(sources);
-    const vdom$ = view(sources.onion.state$, action$);
+    const commentChildren = isolate(CommentCollection, 'comments')(sources);
+
+    const vdom$ = view(sources.onion.state$, commentChildren.DOM, action$);
 
     const sinks = {
         DOM: vdom$,
